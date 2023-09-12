fedca27d323f6df92cd98ac28e14964624c5615f
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-09-12 17:28:44 +0000

    add the create req

--- index.ts
+++ index.ts
@@ -4,14 +4,19 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "jain doe",
+      email: "jain@gmail.com",
+    },
   });
+
+  console.log(user);
+
+  await prisma.$disconnect();
+}
+
+main().catch((e) => {
+  console.error(e);
+});
